e3a3b7357b6a58d67f5586d13968810eb8318ca9
Estebadido <n00272252@upn.pe>
2025-08-24 06:02:11 -0500

    Correcion eliminacion de instancias Ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -16,11 +16,11 @@ resource "aws_instance" "Bastion_Host" {
   subnet_id = var.public_subnets_ids["${var.region}a"]
   private_ip    = "10.0.1.10"
   security_groups = var.sg_bastion_host
-/*
+
 lifecycle {
   ignore_changes = all
 }
-*/
+
 tags = merge(
     var.tags,
     {
@@ -39,11 +39,11 @@ resource "aws_instance" "Instancia_Privada" {
   subnet_id = var.private_subnets_ids["${var.region}b"]
   private_ip    = "10.0.4.10"
   security_groups = var.sg_private_instance
-/*
+
 lifecycle {
   ignore_changes = all
 }
-*/
+
 tags = merge(
     var.tags,
     {
